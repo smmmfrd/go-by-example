16798502de031bf7763f5a5e0689dd075849e716
smmmfrd <smmmfrd@gmail.com>
2023-09-19 15:31:33 -0700

    Add Repetition Parameter

--- 03-iteration/repeat.go
+++ 03-iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(char string) string {
+func Repeat(char string, reps int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < reps; i++ {
 		repeated += char
 	}
 	return repeated
