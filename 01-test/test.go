d293300f5a3cd726e9842164bb009f416177be30
smmmfrd <smmmfrd@gmail.com>
2023-09-18 17:08:06 -0700

    Add Language

--- 01-test/test.go
+++ 01-test/test.go
@@ -4,13 +4,29 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
