4c7ccdb46ee85e5418da53a000457e8db5c18c46
smmmfrd <smmmfrd@gmail.com>
2023-09-19 16:33:41 -0700

    Sum All Test Passing

--- 04-arrays/sum.go
+++ 04-arrays/sum.go
@@ -9,3 +9,19 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNUmbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNUmbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
--- 04-arrays/sum_test.go
+++ 04-arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -25,5 +28,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
