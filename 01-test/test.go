f7be3196f2b72af5138229eedbc792746e005e94
smmmfrd <smmmfrd@gmail.com>
2023-09-18 17:13:32 -0700

    Final Refactor for Hello World

--- 01-test/test.go
+++ 01-test/test.go
@@ -1,30 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
 
-const spanish = "Spanish"
-const spanishHelloPrefix = "Hola, "
+	spanish            = "Spanish"
+	spanishHelloPrefix = "Hola, "
 
-const french = "French"
-const frenchHelloPrefix = "Bonjour, "
+	french            = "French"
+	frenchHelloPrefix = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
+	// Public functions start with a capital!
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
+	// Lowercase functions are internal/private.
+
+	// In the return type, we create a variable that will exist in our function!
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	// Due to our named return value, this actually means = return prefix
+	return
 }
 
 func main() {
