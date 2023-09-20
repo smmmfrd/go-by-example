a670d833142d33c5c1742a2a22e38a34e9fe594b
smmmfrd <smmmfrd@gmail.com>
2023-09-20 13:44:05 -0700

    Sum of Tails Test Pass

--- 04-arrays/sum.go
+++ 04-arrays/sum.go
@@ -25,3 +25,18 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(tailsToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range tailsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
