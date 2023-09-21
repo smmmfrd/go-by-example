5f2ad620bf1ed752df52c2b745588e04511a48b0
smmmfrd <smmmfrd@gmail.com>
2023-09-20 18:59:48 -0700

    Convert Functions to Methods

--- 05-structs/shapes.go
+++ 05-structs/shapes.go
@@ -1,14 +1,24 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rect Rectangle) float64 {
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (rect Rectangle) Perimeter() float64 {
 	return 2 * (rect.Width + rect.Height)
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.Width * rect.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
