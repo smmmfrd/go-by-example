96aff3f0d64008f56d3b7328f3ea90d1562f812d
smmmfrd <smmmfrd@gmail.com>
2023-09-25 17:22:48 -0700

    Withdraw Method and String Method

--- 06-pointers/wallet.go
+++ 06-pointers/wallet.go
@@ -4,6 +4,10 @@ import "fmt"
 
 type Bitcoin int
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	balance Bitcoin
 }
@@ -11,6 +15,10 @@ type Wallet struct {
 func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- 06-pointers/wallet_test.go
+++ 06-pointers/wallet_test.go
@@ -1,23 +1,36 @@
 package wallet
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+		got := wallet.Balance()
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		want := Bitcoin(10)
 
-	want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
